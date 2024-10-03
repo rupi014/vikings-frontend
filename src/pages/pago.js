f0a03a03c4db0df334dbce327b719929e78d2480
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-10-03 10:52:46 +0200

    payment page styles modified

--- src/pages/pago.js
+++ src/pages/pago.js
@@ -12,24 +12,26 @@ const Pago = () => {
   }, []);
 
   return (
-    <div className="pago-container">
-      <h1>Resumen del Pedido</h1>
-      <div className="resumen-pedido">
-        {cesta.length > 0 ? (
-          cesta.map((producto, index) => (
-            <div key={index} className="producto-resumen">
-              <img src={producto.image} alt={producto.name} className="producto-resumen-imagen" />
-              <h2 className="producto-resumen-nombre">{producto.name}</h2>
-              <p className="producto-resumen-precio">{producto.price.toFixed(2)} €</p>
-              <p className="producto-resumen-talla">Talla: {producto.talla || "Talla Única"}</p>
-              <p className="producto-resumen-cantidad">Cantidad: {producto.cantidad}</p>
-            </div>
-          ))
-        ) : (
-          <p>No hay productos en el pedido.</p>
-        )}
-      </div>
-      <button className="pagar-boton">Pagar</button>
+    <div className="pago-page">
+        <div className="pago-container">
+        <h1>Resumen del Pedido</h1>
+        <div className="resumen-pedido">
+            {cesta.length > 0 ? (
+            cesta.map((producto, index) => (
+                <div key={index} className="producto-resumen">
+                <img src={producto.image} alt={producto.name} className="producto-resumen-imagen" />
+                <h2 className="producto-resumen-nombre">{producto.name}</h2>
+                <p className="producto-resumen-precio">{producto.price.toFixed(2)} €</p>
+                <p className="producto-resumen-talla">Talla: {producto.talla || "Talla Única"}</p>
+                <p className="producto-resumen-cantidad">Cantidad: {producto.cantidad}</p>
+                </div>
+            ))
+            ) : (
+            <p>No hay productos en el pedido.</p>
+            )}
+        </div>
+        <button className="pagar-boton">Pagar</button>
+        </div>
     </div>
   );
 };
