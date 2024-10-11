7b14fdfc50275a093a39a7ea789e59d3e10ce2bc
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-10-11 09:08:31 +0200

    payment style fixed

--- src/pages/pago2.js
+++ src/pages/pago2.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
-import './page-styles/pago2.css';
+import './page-styles/pago.css';
 
 const Pago2 = () => {
   const location = useLocation();
